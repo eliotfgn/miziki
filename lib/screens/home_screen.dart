2015d09b393ce2ceaf58f9b1c81a2528c08bf626
Eliot Fagnon <efagnon2002@gmail.com>
2023-02-24 13:34:24 +0100

    change header by app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,28 +12,28 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Découvrir", style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold),),
+          backgroundColor: Colors.transparent,
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage("assets/images/1.jpg"),
+              ),
+            ),
+          ],
+        ),
+
         body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.only(top: 30.0, left: 20),
             child: Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text("Découvrir",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold),),
-                    Padding(
-                      padding: EdgeInsets.only(right: 20.0),
-                      child: CircleAvatar(
-                        radius: 25,
-                        backgroundColor: Colors.red,
-                      ),
-                    ),
-                  ],
-                )
               ],
             ),
           )
