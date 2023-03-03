88ffe01469c88fd19415907ada225d751b77dbbe
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-03 16:57:40 +0100

    update track model

--- lib/models/track.dart
+++ lib/models/track.dart
@@ -1,5 +1,3 @@
-import 'package:music_streaming_app/models/artist.dart';
-
 class Track {
   final int id;
   final String title;
@@ -8,21 +6,18 @@ class Track {
   final String cover;
   final List<String> tags;
   final int likesCount;
-  List<Artist> artists = [];
+  List<int> artists = [];
 
   Track(this.id, this.title, this.duration, this.audio, this.cover, this.tags,
       this.likesCount, this.artists);
 
   factory Track.fromJson(Map<String, dynamic> json) => Track(
-        json['id'],
-        json['title'],
-        json['duration'],
-        json['audio'],
-        json['cover'],
-        json['tags'].cast<String>(),
-        json['likesCount'],
-        json['artists']
-            .map<Artist>((artist) => Artist.fromJson(artist))
-            .toList(),
-      );
+      json['id'],
+      json['title'],
+      json['duration'],
+      json['audio'],
+      json['cover'],
+      json['tags'].cast<String>(),
+      json['likesCount'],
+      json['artists'].cast<int>());
 }
