f001c40d2e0d95941f9496c7324289102ca60906
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-03 11:54:45 +0100

    fetch playlists from api

--- lib/services/PlaylistService.dart
+++ lib/services/PlaylistService.dart
@@ -1,1 +1,39 @@
+import 'package:dio/dio.dart';
+import 'package:music_streaming_app/constants/utils.dart';
+import 'package:music_streaming_app/models/playlist.dart';
 
+class PlaylistService {
+  static Future<List<Playlist>> getPlaylists() async {
+    List<Playlist> playlists = [];
+    Response response;
+
+    try {
+      response = await Dio().get(
+        "${apiMizikiBaseUrl}playlists",
+      );
+      response.data.forEach((playlist) {
+        playlists.add(Playlist.fromJson(playlist));
+      });
+    } on DioError catch (e) {
+      print(e);
+    }
+
+    return playlists;
+  }
+
+  static Future<Playlist?> getPlaylist(int id) async {
+    Playlist? playlist;
+    Response response;
+
+    try {
+      response = await Dio().get(
+        "${apiMizikiBaseUrl}playlists/$id",
+      );
+      playlist = Playlist.fromJson(response.data);
+    } on DioError catch (e) {
+      print(e);
+    }
+
+    return playlist;
+  }
+}
