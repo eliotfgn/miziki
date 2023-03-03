8c63d94efbcbc1c73176a546d5f97b271d47a89a
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-03 11:33:59 +0100

    get playlist tracks list

--- lib/models/playlist.dart
+++ lib/models/playlist.dart
@@ -1,3 +1,5 @@
+import 'package:music_streaming_app/models/track.dart';
+
 class Playlist {
   final int id;
   final String name;
@@ -6,12 +8,12 @@ class Playlist {
   final int likesCount;
   final bool isPrivate;
   final int userId;
+  List<Track> tracks = [];
 
   Playlist(this.id, this.name, this.description, this.cover, this.likesCount,
-      this.isPrivate, this.userId);
+      this.isPrivate, this.userId, this.tracks);
 
-  factory Playlist.fromJson(Map<String, dynamic> json) =>
-      Playlist(
+  factory Playlist.fromJson(Map<String, dynamic> json) => Playlist(
         json['id'],
         json['name'],
         json['description'],
@@ -19,5 +21,6 @@ class Playlist {
         json['likesCount'],
         json['isPrivate'],
         json['userId'],
+        json['tracks'].map<Track>((track) => Track.fromJson(track)).toList(),
       );
 }
