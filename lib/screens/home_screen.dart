eddd0c50a0110f6b67bc5dc51df5a97f16ebb6b1
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-07 00:09:40 +0100

    dynamically fetch informations for home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:music_streaming_app/constants/utils.dart';
+import 'package:music_streaming_app/services/playlist_service.dart';
 import 'package:music_streaming_app/services/track_service.dart';
 import 'package:music_streaming_app/widgets/bottom_navigation.dart';
 
@@ -16,6 +17,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<Track> trendingTracks = [];
   List<Playlist> trendingPlaylists = [];
+  List<Playlist> playlists = [];
 
   void initTracks() async {
     trendingTracks = await TrackService.getTracks();
@@ -24,11 +26,22 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {});
   }
 
+  void initPlaylists() async {
+    playlists = await PlaylistService.getPlaylists();
+    playlists.sort(
+      (a, b) => a.tracks.length.compareTo(b.tracks.length),
+    );
+    trendingPlaylists = playlists.sublist(0, 2);
+    playlists.shuffle();
+    setState(() {});
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     initTracks();
+    initPlaylists();
   }
 
   @override
@@ -62,42 +75,41 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(
                 height: 170,
                 child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    Container(
-                      height: 170,
-                      width: 170,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.red,
-                        image: const DecorationImage(
-                            image: NetworkImage(
-                                "http://10.0.2.2:8081/download/playlist-moody.png"),
-                            fit: BoxFit.fitWidth,
-                            colorFilter: ColorFilter.mode(
-                                Colors.black45, BlendMode.darken)),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: const [
-                          Text(
-                            "Moody",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
+                    scrollDirection: Axis.horizontal,
+                    children: playlists
+                        .map(
+                          (playlist) => Container(
+                            height: 170,
+                            width: 170,
+                            margin: const EdgeInsets.only(right: 20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.red,
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "${apiFileServerBaseUrl}download/${playlist.cover}"),
+                                  fit: BoxFit.fitWidth,
+                                  colorFilter: const ColorFilter.mode(
+                                      Colors.black45, BlendMode.darken)),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(
+                                  playlist.name,
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                              ],
+                            ),
                           ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                  ],
-                ),
+                        )
+                        .toList()),
               ),
               const SizedBox(
                 height: 40,
@@ -133,27 +145,27 @@ class _HomeScreenState extends State<HomeScreen> {
                           const SizedBox(
                             width: 15,
                           ),
-                          const CircleAvatar(
+                          CircleAvatar(
                             radius: 22,
                             backgroundColor: Colors.white,
                             backgroundImage: NetworkImage(
-                                "http://10.0.2.2:8081/download/profile-pic-eliot.png"),
+                                "${apiFileServerBaseUrl}download/${trendingPlaylists[0].cover}"),
                           ),
                           const SizedBox(
                             width: 10,
                           ),
                           Column(
                             mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
+                            children: [
                               Text(
-                                "Love is war",
-                                style: TextStyle(
+                                trendingPlaylists[0].name,
+                                style: const TextStyle(
                                     color: Colors.white,
                                     fontSize: 15,
                                     fontWeight: FontWeight.bold),
                               ),
-                              Text("15 titres",
-                                  style: TextStyle(
+                              Text("${playlists[0].tracks.length} titres",
+                                  style: const TextStyle(
                                       color: Colors.white, fontSize: 12))
                             ],
                           )
@@ -180,27 +192,27 @@ class _HomeScreenState extends State<HomeScreen> {
                           const SizedBox(
                             width: 15,
                           ),
-                          const CircleAvatar(
+                          CircleAvatar(
                             radius: 22,
                             backgroundColor: Colors.white,
                             backgroundImage: NetworkImage(
-                                "http://10.0.2.2:8081/download/playlist-the-vie-radio.jpeg"),
+                                "${apiFileServerBaseUrl}download/${trendingPlaylists[1].cover}"),
                           ),
                           const SizedBox(
                             width: 10,
                           ),
                           Column(
                             mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
+                            children: [
                               Text(
-                                "The vie radio",
-                                style: TextStyle(
+                                trendingPlaylists[1].name,
+                                style: const TextStyle(
                                     color: Colors.white,
                                     fontSize: 15,
                                     fontWeight: FontWeight.bold),
                               ),
-                              Text("21 titres",
-                                  style: TextStyle(
+                              Text("${playlists[1].tracks.length} titres",
+                                  style: const TextStyle(
                                       color: Colors.white, fontSize: 12))
                             ],
                           )
