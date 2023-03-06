ac4d74fa08bfd991deecd2331ac8b2c08a6498a2
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-06 23:51:16 +0100

    get trending songs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:music_streaming_app/constants/utils.dart';
+import 'package:music_streaming_app/services/track_service.dart';
 import 'package:music_streaming_app/widgets/bottom_navigation.dart';
+
+import '../models/playlist.dart';
+import '../models/track.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,6 +14,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Track> trendingTracks = [];
+  List<Playlist> trendingPlaylists = [];
+
+  void initTracks() async {
+    trendingTracks = await TrackService.getTracks();
+    trendingTracks.shuffle();
+    trendingTracks = trendingTracks.sublist(0, 10);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initTracks();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -200,46 +222,49 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(
                   height: 250,
                   child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 170,
-                            width: 170,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.red,
-                                image: const DecorationImage(
-                                    image: NetworkImage(
-                                        "http://10.0.2.2:8081/download/album-burna-boy-love-damini.jpeg"))),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Text(
-                            "Last Last",
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          const Text(
-                            "Burna Boy",
-                            style: TextStyle(
-                                color: Colors.red,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      )
-                    ],
-                  ))
+                      scrollDirection: Axis.horizontal,
+                      children: trendingTracks
+                          .map((track) => Container(
+                                margin: const EdgeInsets.only(right: 20),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      height: 170,
+                                      width: 170,
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          color: Colors.red,
+                                          image: DecorationImage(
+                                              image: NetworkImage(
+                                                  "${apiFileServerBaseUrl}download/${track.cover}"))),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      track.title,
+                                      style: const TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      track.artists[0].name,
+                                      style: const TextStyle(
+                                          color: Colors.red,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
+                              ))
+                          .toList()))
             ],
           ),
         )),
